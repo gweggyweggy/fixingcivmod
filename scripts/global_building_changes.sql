59951b6df3a1920dfc10f57bd596ec62beaec75e
gweggyweggy <unconfigured@null.spigotmc.org>
2021-08-21 16:54:49 -0400

    fixed building sql bug

--- scripts/global_building_changes.sql
+++ scripts/global_building_changes.sql
@@ -2,6 +2,14 @@
 -- Author: Cezas
 -- DateCreated: 8/21/2021 3:36:20 PM
 --------------------------------------------------------------
+
+--City Center
 UPDATE Buildings SET Cost=70 WHERE BuildingType='BUILDING_WATER_MILL';
-UPDATE Buildings SET YieldChange=2 WHERE BuildingType='BUILDING_STABLE' AND YieldType='YIELD_PRODUCTION';
-UPDATE Buildings SET YieldChange=2 WHERE BuildingType='BUILDING_BARRACKS' AND YieldType='YIELD_PRODUCTION';
+
+--Encampments
+UPDATE Building_YieldChanges SET YieldChange=2 WHERE BuildingType='BUILDING_STABLE' AND YieldType='YIELD_PRODUCTION';
+UPDATE Building_YieldChanges SET YieldChange=2 WHERE BuildingType='BUILDING_BARRACKS' AND YieldType='YIELD_PRODUCTION';
+
+--Theatre Square
+UPDATE Building_YieldChanges SET YieldChange=4 WHERE BuildingType='BUILDING_BROADCAST_CENTER';
+UPDATE Building_XP2 SET RequiredPower=2 WHERE BuildingType='BUILDING_BROADCAST_CENTER';
--- scripts/pantheons.sql
+++ scripts/pantheons.sql
@@ -2,7 +2,7 @@
 -- Author: Cezas
 -- DateCreated: 8/18/2021 9:06:59 PM
 --------------------------------------------------------------
-
+--TODO add xml changes
 UPDATE ModifierArguments SET Value='50' WHERE ModifierId='CITY_PATRON_GODDESS_DISTRICT_PRODUCTION_MODIFIER' AND Name='Amount'; --thank you bbg
 UPDATE ModifierArguments SET Value=2 WHERE ModifierId='EARTH_GODDESS_APPEAL_FAITH_MODIFIER' AND Name='Amount'; 
 
