1d0a3d9e1fa8b315c861701d6c6f0312ca3c858f
gweggyweggy <unconfigured@null.spigotmc.org>
2021-09-10 17:28:11 -0400

    hoplite and pike nerf, may revert pike nerf

---
+++ scripts/civ_changes/greece.sql
@@ -0,0 +1,5 @@
+-- greece
+-- Author: Cezas
+-- DateCreated: 9/10/2021 5:08:51 PM
+--------------------------------------------------------------
+UPDATE Units SET Combat=25 WHERE UnitType='UNIT_GREEK_HOPLITE'; 
--- scripts/global_improvement_changes.sql
+++ scripts/global_improvement_changes.sql
@@ -4,3 +4,9 @@
 --------------------------------------------------------------
 UPDATE Improvements SET PrereqTech='TECH_CASTLES' WHERE ImprovementType='IMPROVEMENT_FORT'; 
 UPDATE Improvement_BonusYieldChanges SET BonusYieldChange=2 WHERE PrereqTech='TECH_GUNPOWDER'; 
+
+--attaching the lumber improvement text as a tech description
+UPDATE Technologies SET Description='LOC_TECH_MASS_PRODUCTION_DESCRIPTION' WHERE TechnologyType='TECH_MASS_PRODUCTION';
+INSERT INTO Improvement_BonusYieldChanges (ImprovementType, YieldType, BonusYieldChange, PrereqTech) VALUES 
+	('IMPROVEMENT_LUMBER_MILL','YIELD_PRODUCTION',1,'TECH_MASS_PRODUCTION')
+	;
