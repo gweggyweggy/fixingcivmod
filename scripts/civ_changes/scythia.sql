772d5117888b5a69fbad11ac01851dd6aaede970
gweggyweggy <unconfigured@null.spigotmc.org>
2021-11-08 21:04:06 -0500

    scythia updated, working on georgia now

--- scripts/civ_changes/scythia.sql
+++ scripts/civ_changes/scythia.sql
@@ -3,3 +3,10 @@
 -- DateCreated: 10/28/2021 6:17:02 PM
 --------------------------------------------------------------
 DELETE FROM TraitModifiers WHERE ModifierId='BONUS_VS_WOUNDED_UNITS';
+
+UPDATE ModifierArguments SET Value=50 WHERE ModifierId='TOMYRIS_HEAL_AFTER_DEFEATING_UNIT';
+
+UPDATE Units SET Cost=80, StrategicResource='RESOURCE_HORSES' WHERE UnitType='UNIT_SCYTHIAN_HORSE_ARCHER';
+
+INSERT INTO Units_XP2 (UnitType, ResourceCost, CanEarnExperience, CanFormMilitaryFormation) VALUES
+	('UNIT_SCYTHIAN_HORSE_ARCHER',10, 1, 1);
